e7aeee5cf729e818aecb5879311c8b76b074e62a
OldWhaler <76100109+OldWhaler@users.noreply.github.com>
2022-11-18 12:49:16 +0300

    feat: add basic logic

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,46 @@
+import { useState, useRef } from 'react'
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [textAreaValue, setTextAreaValue] = useState('')
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  function changeTextAreaValue() {
+    const word = inputRef.current?.value as string;
+    setInputValue('')
+
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error()
+        }
+      })
+      .then(
+        data => setTextAreaValue(data[0].word),
+        e => setTextAreaValue('Что-то пошло не так, попробуйте позже')
+      )
+  }
+
+
   return (
     <div className="App">
+      <input
+        type="text"
+        ref={inputRef}
+        value={inputValue}
+        onChange={e => setInputValue(e.target.value)}
+        onKeyDown={e => e.key === 'Enter' ? changeTextAreaValue() : null}
+      />
+      <button
+        onClick={changeTextAreaValue}
+      >get description
+      </button>
+      <textarea
+        defaultValue={textAreaValue}
+      />
     </div>
   );
 }
